94a110487f57b7bef7281796e89deab7250882bf
MishynMykhailo <misham18092001@gmail.com>
2022-09-18 13:39:22 +0300

    done practice-03

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,6 +1,4 @@
 module.exports = (err, req, res, next) => {
-  console.log(res.statusCode);
-  console.log(process.env.NODE_ENV);
   const statusCode = res.statusCode ? res.statusCode : 500;
   res.status(statusCode);
   const environment = process.env.NODE_ENV === "production" ? null : err.stack;
---
+++ backend/models/rolesModel.js
@@ -0,0 +1,16 @@
+const { model, Schema } = require("mongoose");
+
+const roleSchema = Schema(
+  {
+    value: {
+      type: String,
+      unique: true,
+      default: "USER",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+module.exports = model("role", roleSchema);
