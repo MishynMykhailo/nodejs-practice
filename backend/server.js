683d76ae1c3e9f9e44321785b8cefffafa993c87
MishynMykhailo <misham18092001@gmail.com>
2022-09-21 23:08:59 +0300

    practice-04 decide

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const connectDB = require("../config/db");
 const path = require("path");
 const cors = require("cors");
+const { engine } = require("express-handlebars");
 require("colors");
 
 const app = express();
@@ -10,8 +11,17 @@ app.use(cors());
 require("cross-env");
 dotenv.config({ path: path.join(__dirname, "..", "config", ".env") });
 
+// set template engine
+app.engine("handlebars", engine());
+app.set("view engine", "handleras");
+app.set("views", "./views");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/contact", (req, res, next) => {
+  res.render("contact");
+});
 app.use("/api/v1", require("./routes/filmsRoutes"));
 app.use("/users", require("./routes/usersRoutes"));
 app.use(require("./middlewares/badUrlError"));
